0735fdf51c9a6c482c6b53469ddc9edf76c7dc59
asita26 <asita98890@gmail.com>
2023-11-30 10:30:47 +0530

    Changing code to find maximum amongst 3 floats

--- src/MaxNumberFinder.java
+++ src/MaxNumberFinder.java
@@ -1,8 +1,8 @@
 public class MaxNumberFinder {
-    public static Integer findMax(Integer a, Integer b, Integer c) {
-        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+    public static Float findMax(Float a, Float b, Float c) {
+        if (a >= b && a >= c) {
             return a;
-        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
+        } else if (b >= a && b >= c) {
             return b;
         } else {
             return c;
@@ -11,15 +11,15 @@ public class MaxNumberFinder {
 
     public static void main(String[] args) {
         // Test Case 1: Max Number at 1st position
-        Integer max1 = findMax(5, 3, 2);
-        System.out.println("Test Case 1: Max Number at 1st position - Max Number: " + max1);
+        Float maxFloat1 = findMax(5.2f, 3.1f, 2.9f);
+        System.out.println("Test Case 1: Max Number at 1st position - Max Number: " + maxFloat1);
 
         // Test Case 2: Max Number at 2nd position
-        Integer max2 = findMax(1, 8, 4);
-        System.out.println("Test Case 2: Max Number at 2nd position - Max Number: " + max2);
+        Float maxFloat2 = findMax(1.7f, 8.5f, 4.2f);
+        System.out.println("Test Case 2: Max Number at 2nd position - Max Number: " + maxFloat2);
 
         // Test Case 3: Max Number at 3rd position
-        Integer max3 = findMax(2, 4, 9);
-        System.out.println("Test Case 3: Max Number at 3rd position - Max Number: " + max3);
+        Float maxFloat3 = findMax(2.3f, 4.6f, 9.1f);
+        System.out.println("Test Case 3: Max Number at 3rd position - Max Number: " + maxFloat3);
     }
 }
