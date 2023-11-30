8b4f0bb7aecd7005c001fa976d5fe5edc83e4650
asita26 <asita98890@gmail.com>
2023-11-30 10:33:25 +0530

    Refactoring code for finding max string

--- src/MaxNumberFinder.java
+++ src/MaxNumberFinder.java
@@ -1,8 +1,8 @@
 public class MaxNumberFinder {
-    public static Float findMax(Float a, Float b, Float c) {
-        if (a >= b && a >= c) {
+    public static String findMax(String a, String b, String c) {
+        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
             return a;
-        } else if (b >= a && b >= c) {
+        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
             return b;
         } else {
             return c;
@@ -10,16 +10,16 @@ public class MaxNumberFinder {
     }
 
     public static void main(String[] args) {
-        // Test Case 1: Max Number at 1st position
-        Float maxFloat1 = findMax(5.2f, 3.1f, 2.9f);
-        System.out.println("Test Case 1: Max Number at 1st position - Max Number: " + maxFloat1);
+        // Test Case 1: Max String at 1st position
+        String maxString1 = findMax("Apple", "Peach", "Banana");
+        System.out.println("Test Case 1: Max String at 1st position - Max String: " + maxString1);
 
-        // Test Case 2: Max Number at 2nd position
-        Float maxFloat2 = findMax(1.7f, 8.5f, 4.2f);
-        System.out.println("Test Case 2: Max Number at 2nd position - Max Number: " + maxFloat2);
+        // Test Case 2: Max String at 2nd position
+        String maxString2 = findMax("Orange", "Grapes", "Kiwi");
+        System.out.println("Test Case 2: Max String at 2nd position - Max String: " + maxString2);
 
-        // Test Case 3: Max Number at 3rd position
-        Float maxFloat3 = findMax(2.3f, 4.6f, 9.1f);
-        System.out.println("Test Case 3: Max Number at 3rd position - Max Number: " + maxFloat3);
+        // Test Case 3: Max String at 3rd position
+        String maxString3 = findMax("Cherry", "Blueberry", "Strawberry");
+        System.out.println("Test Case 3: Max String at 3rd position - Max String: " + maxString3);
     }
 }
