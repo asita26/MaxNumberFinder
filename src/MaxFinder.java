cc7e425fbd6e436e89df130c8cea4c4156249790
asita26 <asita98890@gmail.com>
2023-11-30 10:46:04 +0530

    Printing max

--- src/MaxFinder.java
+++ src/MaxFinder.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,7 +10,9 @@ public class MaxFinder<T extends Comparable<T>> {
     }
 
     public T testMaximum() {
-        return findMax(values);
+        T max = findMax(values);
+        printMax(max);
+        return max;
     }
 
     public static <U extends Comparable<U>> U findMax(U... values) {
@@ -23,21 +24,25 @@ public class MaxFinder<T extends Comparable<T>> {
         return max.orElse(null);
     }
 
+    public static <V> void printMax(V max) {
+        System.out.println("Maximum Value: " + max);
+    }
+
     public static void main(String[] args) {
         // Test Case 1: Max Number at 1st position
         MaxFinder<Integer> maxFinder1 = new MaxFinder<>(5, 3, 2);
-        System.out.println("Test Case 1: Max Number at 1st position - Max Number: " + maxFinder1.testMaximum());
+        maxFinder1.testMaximum();
 
         // Test Case 2: Max Number at 2nd position
         MaxFinder<Float> maxFinder2 = new MaxFinder<>(1.7f, 8.5f, 4.2f);
-        System.out.println("Test Case 2: Max Number at 2nd position - Max Number: " + maxFinder2.testMaximum());
+        maxFinder2.testMaximum();
 
         // Test Case 3: Max String at 3rd position
         MaxFinder<String> maxFinder3 = new MaxFinder<>("Cherry", "Blueberry", "Strawberry");
-        System.out.println("Test Case 3: Max String at 3rd position - Max String: " + maxFinder3.testMaximum());
+        maxFinder3.testMaximum();
 
         // Test Case 4: Max Number with more than three parameters
         MaxFinder<Double> maxFinder4 = new MaxFinder<>(3.5, 6.2, 8.9, 2.1, 5.7);
-        System.out.println("Test Case 4: Max Number with more than three parameters - Max Number: " + maxFinder4.testMaximum());
+        maxFinder4.testMaximum();
     }
 }
